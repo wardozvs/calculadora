bc523f79656e37b065b3503d4994880456bc25fc
Yuli <brenda.03pro@gmail.com>
2024-10-02 18:47:09 -0600

    HTLM/CSS

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 function addInputs() {
-    const input1 = document.getElementById('input1').value;
-    const input2 = document.getElementById('input2').value;
+    const num1 = document.getElementById('num1').value;
+    const num2 = document.getElementById('num2').value;
 
-    const sum = parseFloat(input1) + parseFloat(input2);
+    const sum = parseFloat(num1) + parseFloat(num2);
 
     document.getElementById('result').textContent = sum;
+}
+
+function substractNumbers(){
+    const num1 = document.getElementById('num1').value;
+    const num2 = document.getElementById('num2').value;
+
+    const subs = parseFloat(num1) - parseFloat(num2);
+
+    document.getElementById('result').textContent = subs;
 }
